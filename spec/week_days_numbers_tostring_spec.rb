b14d34234537ac12bdd0c8db4128277b4d16a63b
tamerl <eng.tamereid@gmail.com>
2019-12-09 23:10:59 +1100

    Adding more passed tests

--- spec/week_days_numbers_tostring_spec.rb
+++ spec/week_days_numbers_tostring_spec.rb
@@ -11,5 +11,25 @@ describe 'week_days_numbers_tostring' do
       result = convert_to_string_from("1,2,3,6,7")
       expect(result).to eq("Mon-Wed, Sat, Sun")
     end
+    it "returns the answer Mon, Wed-Sat when the input is 1,3,4,5,6" do
+      result = convert_to_string_from("1,3,4,5,6")
+      expect(result).to eq("Mon, Wed-Sat")
+    end
+    it "returns the answer Tue-Thu, Sat, Sun when the input is 2,3,4,6,7" do
+      result = convert_to_string_from("2,3,4,6,7")
+      expect(result).to eq("Tue-Thu, Sat, Sun")
+    end
+    it "returns the answer Mon, Wed, Thu, Sat, Sun when the input is 1,3,4,6,7" do
+      result = convert_to_string_from("1,3,4,6,7")
+      expect(result).to eq("Mon, Wed, Thu, Sat, Sun")
+    end
+    it "returns the answer Sun when the input is 7" do
+      result = convert_to_string_from("7")
+      expect(result).to eq("Sun")
+    end
+    it "returns the answer Mon, Sun when the input is 1,7" do
+      result = convert_to_string_from("1,7")
+      expect(result).to eq("Mon, Sun")
+    end
   end
 end
